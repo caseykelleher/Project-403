35cd5eb18c7514e47d78a14824aa4b936816591c
Casey K <caseykelleher@icloud.com>
2020-03-27 23:33:39 -0400

    frontend

--- pro-day-fitness/backend/server.js
+++ pro-day-fitness/backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.Port || 3000;
+const port = process.env.Port || 5000;
 
 app.use(cors());
 app.use(express.json());
